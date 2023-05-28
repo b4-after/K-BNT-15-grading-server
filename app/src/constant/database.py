eb2d06130f38ff12a4fecf14b563153a76e251e2
taehyun <0417taehyun@gmail.com>
2023-05-28 17:09:06 +0900

    fix (database): SQL 쿼리 파일 위치 수정

--- app/src/constant/database.py
+++ app/src/constant/database.py
@@ -6,14 +6,13 @@ from pydantic import FilePath
 
 
 class AnswerQuery(str, Enum):
-    _BASE_PATH: str = "sql/answer"
+    _BASE_PATH: str = "var/task/root/src/sql/answer"
     GET_BY_OBJECT_KEY: str = "get_by_object_key.sql"
     UPDATE_ANSWER_STATUS: str = "update_answer_status.sql"
 
     @DynamicClassAttribute
     def value(self) -> FilePath:
-        _CURRENT_DIRECTORY_PATH: FilePath = os.path.dirname(os.path.abspath(__file__))
-        return os.path.join(_CURRENT_DIRECTORY_PATH, "/".join([AnswerQuery._BASE_PATH, self._value_]))
+        return "/".join([AnswerQuery._BASE_PATH, self._value_])
 
 
 class AnswerStatus(str, Enum):
